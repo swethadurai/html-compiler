6afd93d485c781d6dfb4cb69e6dbc82bfe4cf730
swethadurai <111978148+swethadurai@users.noreply.github.com>
2024-04-23 18:33:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function compileHtml(field,e){
+    document.getElementById('output').innerHTML=field.value;
+}
+
+function compileCss(field,e){
+    document.getElementById('styling').innerHTML=field.value;
+}
